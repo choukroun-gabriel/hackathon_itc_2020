8351dd3993870c0143377d7c406b22831d8179f9
Yaniv Goldfrid <ygoldfrid@gmail.com>
2021-01-20 16:40:01 +0200

    updated api to scale values

--- InferenceServer.py
+++ InferenceServer.py
@@ -10,24 +10,56 @@ import sklearn
 app = Flask(__name__)
 
 
+def min_max_scale(value, min, max):
+    return (float(value) - min) / (max - min)
+
+
+def dummy_gender(value):
+    if value.lower() == 'female':
+        return 0
+    if value.lower() == 'male':
+        return 1
+
+
+def dummy_company(value):
+    if value.lower() == 'product':
+        return 0
+    if value.lower() == 'service':
+        return 1
+
+
+def dummy_wfh(value):
+    if value.lower() == 'no':
+        return 0
+    if value.lower() == 'yes':
+        return 1
+
+
 @app.route('/predict')
-def predict_single():
+def predict():
     with open('stress_model.pkl', 'rb') as input_file:
         gnb = load(input_file)
 
-    fatigue = request.args.get('fatigue')
-    designation = request.args.get('designation')
-    hours_worked = request.args.get('hours_worked')
-    gender = request.args.get('gender')
-    company_type = request.args.get('company_type')
-    wfh_setup = request.args.get('wfh_setup')
-    array = np.reshape([fatigue, designation, hours_worked, gender, company_type,
-                        wfh_setup], (1, -1))
+    # Scaling
+    fatigue = min_max_scale(request.args.get('fatigue'), min=0, max=10)
+    designation = min_max_scale(request.args.get('designation'), min=0, max=5)
+    hours_worked = min_max_scale(request.args.get('hours_worked'), min=1, max=10)
 
+    # Dumming
+    gender = dummy_gender(request.args.get('gender'))
+    company_type = dummy_company(request.args.get('company_type'))
+    wfh_setup = dummy_wfh(request.args.get('wfh_setup'))
+
+    array = np.reshape([fatigue, designation, hours_worked, gender, company_type, wfh_setup], (1, -1))
     result = gnb.predict(array.astype(float))
+
     return str(result)
 
 
 if __name__ == '__main__':
     port = os.environ.get('PORT')
-    app.run(host='0.0.0.0', port=int(port))
+
+    if port:
+        app.run(host='0.0.0.0', port=int(port))
+    else:
+        app.run(host='0.0.0.0')
